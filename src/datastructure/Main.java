0f2731eb046d138271dedc13c8820963801e4508
Guavaer <zhaojunjie.mike@gmail.com>
2017-04-14 10:22:09 +0800

    init commit

---
+++ src/datastructure/Main.java
@@ -0,0 +1,13 @@
+package datastructure;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+         int n =100; int sum = 0;
+      // data model will be always like [0,n-1]
+        for (int i = 0; i <= n-1; i++) {
+            System.out.println(i);
+        }
+    }
+}
