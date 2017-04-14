cdbaa201ab51cb243cafe3854d875001a0e359cf
Guavaer <zhaojunjie.mike@gmail.com>
2017-04-14 10:39:46 +0800

    init commit

--- src/datastructure/Main.java
+++ src/datastructure/Main.java
@@ -4,10 +4,49 @@ public class Main {
 
     public static void main(String[] args) {
 
-         int n =100; int sum = 0;
-      // data model will be always like [0,n-1]
-        for (int i = 0; i <= n-1; i++) {
-            System.out.println(i);
+//        singleForLoop();
+
+//        doubleForLoop();
+
+
+        tribleForLoop();
+
+    }
+
+    private static void tribleForLoop() {
+        int k = 0;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 4; j++) {
+                for (int h = 0; h < 5; h++) {
+                    System.out.println(k++);
+                    System.out.println(++k);
+                }
+            }
         }
+
+        //time complexity is O(n3)
+    }
+
+    private static void singleForLoop() {
+        int n = 100;
+        int sum = 0;
+        // data model will be always like [0,n-1]
+        for (int i = 0; i <= n - 1; i++) { //executed N+1 times
+            System.out.println(i); // executed N times
+        }
+
+        // the total executed times is 2N+1, then time complexity is O(n)
+    }
+
+    private static void doubleForLoop() {
+        int k = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.println(k++);
+                System.out.println(++k);
+            }
+
+        }
+        //time complexity is O(n2)
     }
 }
